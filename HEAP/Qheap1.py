e8aeaff5ad676b0423185cf35a5121e5c7569a6f
sushilpoddar <60999317+sushilpoddar@users.noreply.github.com>
2020-09-16 13:27:41 +0530

    Create Qheap1.py

---
+++ HEAP/Qheap1.py
@@ -0,0 +1,18 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import heapq as hq
+from bisect import insort
+
+if __name__ == "__main__":
+    t = int(input().strip())
+    q = []
+    
+    for _ in range(t):
+        args = input().strip().split()
+        if args[0] == '1':
+            insort(q, int(args[1]))
+        
+        elif args[0] == '2':
+            q.remove(int(args[1]))
+
+        elif args[0] == '3':
+            print(q[0])
