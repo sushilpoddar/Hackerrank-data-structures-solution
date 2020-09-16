37cca2e2db4f165e3a3894e4c691b7b93bcd3e6a
sushilpoddar <60999317+sushilpoddar@users.noreply.github.com>
2020-09-16 12:12:49 +0530

    Create Insert_a_node_at_the_specific_position_in_a_linked_list.py

---
+++ LINKED LIST/Insert_a_node_at_the_specific_position_in_a_linked_list.py
@@ -0,0 +1,83 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtPosition function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtPosition(head, data, position):
+    if head is None:
+        head = SinglyLinkedListNode(data)
+    else:
+        iterator = head
+        for i in range(0, position+1):
+            if i == position:
+                node = SinglyLinkedListNode(data)
+                node.next = iterator
+                iterator1.next = node        
+            iterator1 = iterator
+            iterator = iterator.next    
+    return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    data = int(input())
+
+    position = int(input())
+
+    llist_head = insertNodeAtPosition(llist.head, data, position)
+
+    print_singly_linked_list(llist_head, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
